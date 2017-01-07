9ff790206f5880990487f555a3eaa606c5b26f3a
Basavaraj <rajiff@gmail.com>
2017-01-07 07:40:56 +0000

    test commit

---
+++ hello.js
@@ -0,0 +1,14 @@
+/*eslint-env node*/
+console.log("Running my first eclipse che program");
+
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Hello World\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(3000);
